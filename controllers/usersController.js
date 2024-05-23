e63be9e4481525c98a7f5cafe32a09117ad4151c
Andrea Natale <andreanatale@iMac-di-Andrea.fritz.box>
2024-05-23 15:47:49 +0200

    Login system controller and route implemented

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,7 +3,6 @@ import { UserModel } from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
 
 export const addNewUser = async (req, res) => {
-    console.log('here')
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const email = req.body.email;
     const isUserExists = await UserModel.findOne({ email: email });
@@ -19,7 +18,7 @@ export const addNewUser = async (req, res) => {
             password: hashedPassword
         })
         const user = newUser.save();
-    
+
         const token = jwt.sign({
             id: user._id,
             email: user.email
@@ -28,7 +27,7 @@ export const addNewUser = async (req, res) => {
             id: user._id,
             email: user.email
         };
-    
+
         res.status(201).send({
             statusCode: 201,
             message: "Nuovo utente inserito correttamente",
@@ -45,18 +44,42 @@ export const addNewUser = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    const {email} = req.body;
-    lowerEmail = email.toLowerCase();
+    const { email } = req.body;
+    const lowerEmail = email.toLowerCase();
     try {
-        const user = await UserModel.findOne({email: lowerEmail});
-        if(!user) {
+        const user = await UserModel.findOne({ email: lowerEmail });
+        if (!user) {
             return res.status(404).send({
                 statusCode: 404,
-                message: `Nessun utente con email ${email} trovato.`
+                message: `Nome utente o password non validi.`
+            })
+        };
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            const token = jwt.sign({
+                id: user._id,
+                email: user.email
+            }, process.env.JWT_SECRET, { expiresIn: '24h' });
+            const userData = {
+                id: user._id,
+                email: user.email
+            };
+            res.status(200).send({
+                statusCode: 200,
+                message: "Login effettuato con successo",
+                payload: { token, userData }
+            })
+        } else {
+            res.status(401).send({
+                statusCode: 401,
+                message: "Nome utente o password non validi."
             })
         }
-        /* if (await bcrypt.compare(req.body)) */
     } catch (error) {
-        //
+        console.error(error);
+        res.status(500).send({
+            statusCode: 500,
+            message: "Internal Server Error",
+            error: error
+        })
     }
 }
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { addNewUser } from '../controllers/usersController.js';
+import { addNewUser, login } from '../controllers/usersController.js';
 
 const usersRoute = express.Router();
 
 usersRoute.post("/register", addNewUser);
+usersRoute.post("/login", login);
 
 export default usersRoute;
