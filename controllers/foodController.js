4012d64fd4610e757ed10a545785b90c65de3255
Andrea Natale <andreanatale@iMac-di-Andrea.local>
2024-05-28 14:33:14 +0200

    food controller and router

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -24,3 +24,108 @@ export const AddNewFood = async (req, res) => {
         })
     }
 }
+
+export const getAllFood = async (req, res) => {
+    try {
+        const food = await FoodModel.find();
+        res.status(200).send({
+            statusCode: 200,
+            payload: food
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            statusCode: 500,
+            message: 'Internal Server Error',
+            error: error
+        })
+    }
+}
+
+export const getFoodById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        if (id) {
+            const food = await FoodModel.findById(id);
+            if (!food) {
+                return res.statuts(404).send({
+                    statusCode: 404,
+                    message: `Nessun oggetto con id ${id} trovato.`
+                })
+            }
+            res.status(200).send({
+                statusCode: 200,
+                payload: food
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            statusCode: 500,
+            message: 'Internal Server Error',
+            error: error
+        })
+    }
+}
+
+export const editFood = async (req, res) => {
+    const { id } = req.params;
+    if (id) {
+        try {
+            const newFood = await FoodModel.findByIdAndUpdate(
+                id,
+                {
+                    name: req.body.name,
+                    course: req.body.course,
+                    price: req.body.price,
+                    allergens: req.body.allergens,
+                    notes: req.body.notes
+                },
+                { new: true }
+            )
+            if (!newFood) {
+                res.status(404).send({
+                    statusCode: 404,
+                    message: `Nessun elemento con id ${id} trovato.`
+                })
+            }
+            res.status(200).send({
+                statusCode: 200,
+                messagge: `Oggetto con id ${id} aggiornato correttamente.`,
+                payload: newFood
+            })
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({
+                statusCode: 500,
+                message: 'Internal Server Error',
+                error: error
+            })
+        }
+    }
+}
+
+export const deleteFood = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const food = await FoodModel.findByIdAndDelete(id);
+        if (!food) {
+            res.status(404).send({
+                statusCode: 404,
+                message: `Nessun oggetto con id ${id} trovato.`
+            })
+        }
+        res.status(200).send({
+            statusCode: 200,
+            messaggio: `Oggetto con id ${id} eliminato correttamente.`
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            statusCode: 500,
+            message: 'Internal Server Error',
+            error: error
+        })
+    }
+
+}
--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { AddNewFood } from '../controllers/foodController.js';
+import { AddNewFood, deleteFood, editFood, getAllFood, getFoodById } from '../controllers/foodController.js';
 
 const foodRoute = express.Router();
 
+foodRoute.get("/", getAllFood);
+foodRoute.get("/:id", getFoodById);
 foodRoute.post("/new-food", AddNewFood);
+foodRoute.patch("/edit/:id", editFood);
+foodRoute.delete("/delete/:id", deleteFood);
 
 export default foodRoute;
